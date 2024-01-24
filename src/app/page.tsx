2e21dac8d1ce1da1fb05c8dfb840f1fcd1bc7cc9
kimnopal <naufalhakim366@gmail.com>
2024-01-24 20:49:55 +0700

    feat: add icon variant into input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import PostCard from "@/components/post-card";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { LucideSearch } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <div>
+        <Input
+          placeholder="Search posts"
+          variant="icon"
+          icon={<LucideSearch size={20} />}
+        />
+      </div>
+      <div className="flex gap-2">
+        <Badge variant={"missing"}>missing</Badge>
+        <Badge>found</Badge>
+      </div>
       <PostCard post={{ slug: "test" }} />
       <PostCard post={{ slug: "test" }} />
       <PostCard post={{ slug: "test" }} />
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,35 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { LucideSearch } from "lucide-react";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: "icon";
+  icon?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, variant, icon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
+      <div className="flex relative items-center">
+        <div className="absolute left-0 w-10 flex justify-center text-zinc-400">
+          {variant == "icon" && icon}
+        </div>
+        <input
+          type={type}
+          className={cn(
+            variant == "icon" ? "pr-3 pl-10" : "px-3",
+            "flex h-10 w-full rounded-md border border-zinc-200 bg-white py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
